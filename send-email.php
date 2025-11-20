947b23d4df0812cc4445da516e709ef34d0cfe45
SP Solutions <spsolutionsapps@gmail.com>
2025-11-20 11:31:35 -0300

    php sender 2

--- send-email.php
+++ send-email.php
@@ -1,4 +1,8 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
